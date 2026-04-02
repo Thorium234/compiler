be6f953aec01c92ea7c10ef0d7b6d1f08c645766
Thorium234 <inyangalapaul5@gmail.com>
2026-04-03 00:06:35 +0300

    Implement Phases 1-6 of Campus Code Lab

--- config/urls.py
+++ config/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/users/login/', permanent=False), name='index'),
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
     path('assignments/', include('assignments.urls')),
